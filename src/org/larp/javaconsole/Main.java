65eae60e4d810e9fae8a10f9e0cbe1e9abe08b65
LuigiR0jas <xluigix98@gmail.com>
2016-07-14 12:15:04 -0400

    Added IOException to main

--- src/org/larp/javaconsole/Main.java
+++ src/org/larp/javaconsole/Main.java
@@ -1,9 +1,13 @@
-package org.larp.javaconsole;
+package org.larp.JavaConsoleLast;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		try{
 		Console console = new Console();
 		console.start();
+	} catch (Exception e){
+		e.printStackTrace();
+		}
 	}
 }
